80dd9ba508e7efc1c01031f936231ebe165c1465
dev-iskan <successfuliskan@gmail.com>
2020-02-17 02:34:03 +0500

    crud for brand

--- app/Http/Controllers/ApiAdmin/Products/BrandController.php
+++ app/Http/Controllers/ApiAdmin/Products/BrandController.php
@@ -3,32 +3,51 @@
 namespace App\Http\Controllers\ApiAdmin\Products;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\Brands\StoreBrandRequest;
+use App\Models\Products\Brand;
 use Illuminate\Http\Request;
 
 class BrandController extends Controller
 {
-    public function index()
+    public function index(Request $request)
+    {
+        $brandsQuery = Brand::latest();
+
+        if ($q = $request->query('q')) {
+            $brandsQuery->where('name', 'like', '%' . $q . '%');
+        }
+
+        if ($request->query('paginate') == true) {
+            return $brandsQuery->paginate($request->offset ?? 10);
+        }
+
+        return $brandsQuery->limit($request->limit ?? null)->get();
+    }
+
+    public function store(StoreBrandRequest $request)
+    {
+        $brand = Brand::create($request->all());
+        return $brand;
+    }
+
+    public function show($id)
     {
 
     }
 
-    public function store()
+    public function update(StoreBrandRequest $request, $id)
     {
+        $brand = Brand::findOrFail($id);
+        $brand->update($request->all());
 
+        return $brand;
     }
 
-    public function show()
+    public function destroy($id)
     {
+        $brand = Brand::findOrFail($id);
+        $brand->delete();
 
-    }
-
-    public function update()
-    {
-
-    }
-
-    public function destroy()
-    {
-
+        return response()->json(['message' => __('response.deleted')]);
     }
 }
