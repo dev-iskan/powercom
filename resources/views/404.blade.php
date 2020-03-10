0484a5566572f707c4d1fc65e7b36d0c75267bc9
AvazAlimov <avazalimov95@mail.ru>
2020-03-10 22:55:32 +0500

    feat: add basic 404 page

---
+++ resources/views/404.blade.php
@@ -0,0 +1,14 @@
+@extends('layout.index')
+
+@section('meta')
+    <meta name="description" content="Powercom.uz">
+    <meta name="og:title" property="og:title" content="Powercom.uz">
+    <meta name="twitter:card" content="Powercom.uz">
+@endsection
+
+@section('body')
+<section class="section is-large has-text-centered">
+    <h1 class="title">Powercom.uz</h1>
+    <p>Страница не найдена</p>
+</section>
+@endsection
