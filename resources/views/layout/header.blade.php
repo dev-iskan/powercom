dad3c2b68c2b27738374b7ae4435ad230f4929c1
AvazAlimov <avazalimov95@mail.ru>
2020-03-16 15:10:32 +0500

    fix: remove categories scrolling

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -34,7 +34,7 @@
     </div>
 </nav>
 <hr style="margin: 0;"/>
-<nav id="catigories" class="navbar level box is-paddingless is-radiusless is-marginless" style="overflow-x: auto;">
+<nav id="catigories" class="navbar level box is-paddingless is-radiusless is-marginless">
     @foreach ($categories as $category)
         <div class="level-item">
             <div class="navbar-item has-dropdown is-hoverable is-mega" style="height: 100%;">
