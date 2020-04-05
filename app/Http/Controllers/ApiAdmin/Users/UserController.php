4b6a57076a95a19691aded099d1613ef36ce6493
dev-iskan <successfuliskan@gmail.com>
2020-04-05 14:05:08 +0500

    fixed update 2

--- app/Http/Controllers/ApiAdmin/Users/UserController.php
+++ app/Http/Controllers/ApiAdmin/Users/UserController.php
@@ -78,7 +78,7 @@ class UserController extends Controller
         }
         $user->update($data);
 
-        if($client =$user->client && $request->phone) {
+        if (($client = $user->client) && $request->phone) {
             $client->update([
                 'phone' => $request->phone
             ]);
